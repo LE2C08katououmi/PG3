63163f739ea5df082cac5c5ff04958b698851bb2
oumi0310 <k021g1382@g.neec.ac.jp>
2022-10-11 16:05:34 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,41 +1,21 @@
 #include<stdio.h>
-#include<typeinfo>
-#include<iostream>
-using namespace std;
 
-template <typename Type>
-
-Type comparison(Type a, Type b) 
+int func(int n)
 {
-	if (a > b)
-	{
-		return a;
-	}
+	if (n == 1)
+		return 1;
 	else
-	{
-		return b;
-	}
+		return func(n - 1) * n;
 }
 
 int main()
 {
-	// intŒ^‚Ì’l‚ğ”äŠr
-	int x = 10;
-	int y = 20;
+	int data;
 
-	cout << "x" << x << "‚Æy:" << y << "‚ğ”äŠr" << endl;
-	auto a = max(x, y);
-	cout << "‘å‚«‚¢’l‚Í" << a << endl;
-	cout << "a‚ÌŒ^ : " << typeid(a).name() << endl;
+	printf("data : ");
+	scanf_s("%d", &data);
 
-	// floatŒ^‚Ì’l‚ğ”äŠr
-	float z = 12.3f;
-	float w = 45.6f;
-
-	cout << "z" << z << "‚Æw:" << w << "‚ğ”äŠr" << endl;
-	auto b = max(z, w);
-	cout << "‘å‚«‚¢’l‚Í" << b << endl;
-	cout << "b‚ÌŒ^ : " << typeid(b).name() << endl;
+	printf("%d != %d\n", data,func(data));
 
 	return 0;
 }
