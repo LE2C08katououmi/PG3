af2a872d20f3a45dec769767a8a4e4f76e3e386e
oumi0310 <k021g1382@g.neec.ac.jp>
2022-11-07 23:19:16 +0900

    課題終了

--- main.cpp
+++ main.cpp
@@ -1,57 +1,57 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<windows.h>
-#include<time.h>
+#include<Windows.h>
+#include<functional>
+#include<random>
 
-typedef void (*PFunc)(int*);
+std::random_device rnd;
+std::mt19937 mt(rnd());
+std::uniform_int_distribution<int> randDice(1, 6);
 
-int odice;
-int dice;
-int cdice;
-
-void callback1(int* s)
+int main(int arge, const char* argv[])
 {
-	if (cdice == 1 && dice == 1)
+	int dice = randDice(mt);
+	int num;
+
+	std::function<int()> diceNum = [&]() {return dice; };
+
+	std::function<int(int)> cNum = [](int diceNum)
 	{
-		printf("正解 diceは%d",odice);
-	}
-	else if (cdice == 2 && dice == 0)
+		if (diceNum % 2 == 0)
+		{
+			return 0;
+		}
+		else if (diceNum % 2 == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return 2;
+		}
+	};
+
+	printf("奇数なら1、偶数なら0を入力してください:");
+	scanf_s("%d", &num);
+
+	auto print = [&](int cNum)
 	{
-		printf("正解 diceは%d",odice);
-	}
-	else
-	{
-		printf("不正解 diceは%d",odice);
-	}
-}
+		Sleep(3 * 1000);
 
-void setTimeout(PFunc p, int second)
-{
-	Sleep(second * 1000);
+		if (cNum == 0 && num == 0)
+		{
+			printf("正解 出た目は%d", dice);
+		}
+		else if (cNum == 1 && num == 1)
+		{
+			printf("正解 出た目は%d",dice);
+		}
+		else
+		{
+			printf("不正解 出た目は%d", dice);
+		}
+	};
 
-	p(&second);
-}
-
-int main()
-{
-	srand(time(nullptr));
-	dice = rand() % 6 + 1;
-	odice = dice;
-
-	printf("奇数なら1、偶数なら2:");
-
-	while (dice > 1)
-	{
-		dice = dice % 2;
-
-		break;
-	}
-
-	scanf_s("%d", &cdice);
-
-	PFunc p;
-	p = callback1;
-	setTimeout(p, 3);
+	print(cNum(diceNum()));
 
 	return 0;
 }
