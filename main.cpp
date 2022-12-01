56881d9b18210ba0f59463df607cafe03bb92da3
oumi0310 <k021g1382@g.neec.ac.jp>
2022-12-01 16:10:29 +0900

    課題だけ

--- main.cpp
+++ main.cpp
@@ -1,57 +1,16 @@
-#include<stdio.h>
-#include<Windows.h>
-#include<functional>
-#include<random>
+#include <stdlib.h>
+#include <list>
+using namespace std;
 
-std::random_device rnd;
-std::mt19937 mt(rnd());
-std::uniform_int_distribution<int> randDice(1, 6);
-
-int main(int arge, const char* argv[])
+int main()
 {
-	int dice = randDice(mt);
-	int num;
+	list<int> lst(33, 4);
+	list<int>::iterator itr;
 
-	std::function<int()> diceNum = [&]() {return dice; };
-
-	std::function<int(int)> cNum = [](int diceNum)
-	{
-		if (diceNum % 2 == 0)
-		{
-			return 0;
-		}
-		else if (diceNum % 2 == 1)
-		{
-			return 1;
-		}
-		else
-		{
-			return 2;
-		}
-	};
-
-	printf("奇数なら1、偶数なら0を入力してください:");
-	scanf_s("%d", &num);
-
-	auto print = [&](int cNum)
-	{
-		Sleep(3 * 1000);
-
-		if (cNum == 0 && num == 0)
-		{
-			printf("正解 出た目は%d", dice);
-		}
-		else if (cNum == 1 && num == 1)
-		{
-			printf("正解 出た目は%d",dice);
-		}
-		else
-		{
-			printf("不正解 出た目は%d", dice);
-		}
-	};
-
-	print(cNum(diceNum()));
+	itr = lst.begin();
+	itr = lst.end();
+	itr--;
+	itr--;
 
 	return 0;
 }
