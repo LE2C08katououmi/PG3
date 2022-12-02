f954ae144126fef5c5555e115e53bde6f7538334
oumi0310 <k021g1382@g.neec.ac.jp>
2022-12-02 13:11:17 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,16 +1,57 @@
 #include <stdlib.h>
 #include <list>
+#include <iterator>
+#include <string>
+#include <iostream>
+
 using namespace std;
 
 int main()
 {
-	list<int> lst(33, 4);
-	list<int>::iterator itr;
+	list<const char*>Station = { "東京 ","神田 ","秋葉原 ","御徒町 ","上野 ","鶯谷 ",
+								"日暮里 ","田端 ","駒込 ","巣鴨 ","大塚 " ,"池袋 ",
+								"目白 ","高田馬場 ","新大久保 ","新宿 ","代々木 ",
+								"原宿 ","渋谷 ","恵比寿 ","目黒 ","五反田 ","大崎 ",
+								"品川 ","田町 ","浜松町 ","新橋 ","有楽町\n\n" };
 
-	itr = lst.begin();
-	itr = lst.end();
-	itr--;
-	itr--;
+	cout << "1970年\n";
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s", *itr);
+	}
+	
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "田端 ")
+		{
+			itr = Station.insert(itr, "西日暮里 ");
+			itr++;
+		}
+	}
+
+	cout << "2019年\n";
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s", *itr);
+	}
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "田町 ")
+		{
+			itr = Station.insert(itr, "高輪ゲートウェイ ");
+			itr++;
+		}
+	}
+
+	cout << "2022年\n";
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s", *itr);
+	}
 
 	return 0;
 }
